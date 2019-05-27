9afb23dfabd7be97c0e438938c33e16dc8a750a5
Ryan Guo <guohao_lin@msn.com>
2019-05-26 23:15:29 -0700

    Function Array Problem

---
+++ FunctionArrayProblem/CArrayFunctionPractise_stl.cpp
@@ -0,0 +1,211 @@
+// The purpose of this program is to provide a way for the student to practise passing
+// arrays to functions and have those functions perform some relatively simple tasks.
+
+// You will need to fill in the missing statements that invoke the functions and the
+// implementations of the 6 functions. Each function can be implemented in 4 to 7 lines
+// of code {not including opening and closing braces}.
+
+// Created by: Bob Langelaan
+// Date:       July 1, 2018
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <array>
+using namespace std;
+
+const unsigned int SIZE1 = 10;
+const unsigned int SIZE2 = 15;
+
+// Function prototypes
+
+// Search array and return the max value in the array
+template <size_t N>
+int arr_max(const array<int, N> &a);
+
+// Search array and return the index of the max value in the array
+template <size_t N>
+size_t arr_index_of_first_max(const array<int, N> &a);
+
+// Search array and return the index of the last max value in the array
+template <size_t N>
+size_t arr_index_of_last_max(const array<int, N> &a);
+
+// Search array and return index of first occurence of value in the array
+// Return -1 if value is not in the array
+template <size_t N>
+size_t arr_index_of_value_first(const array<int, N> &a, int value);
+
+// Search array and return index of last occurence of value in the array
+// Return -1 if value is not in the array
+template <size_t N>
+size_t arr_index_of_value_last(const array<int, N> &a, int value);
+
+// Compare 2 arrays and return true if they are equal and false otherwise
+// You can assume that the 2 arrays are the same size
+template <size_t N>
+bool arr_compare(const array<int, N> &a, const array<int, N> &b);
+
+
+
+int main()
+{
+	array<int, SIZE1> arr1 = { 10, 20, 75, 15, 35, 12, 65, 15, 75, 55};
+	array<int, SIZE1> arr2 = { 10, 20, 75, 15, 35, 12, 65, 15, 75, 55 };
+	array<int, SIZE1> arr3 = { 10, 20, 75, 15, 100, 12, 65, 15, 75, 55 };
+
+	size_t result = 0; // used to collect result from function returning a size_t value
+	int result2 = 0;  // used to collect result from function returning an int value
+	bool result3 = false; // used to collect result from function returning a bool value
+
+	// Test arr_max() function
+	result2 = arr_max(arr1); // I am providing the first invocation, you supply the rest :)
+	cout << "The max value in the array \"arr1\" is: " << result2;
+
+	// Test arr_index_of_first_max() function
+    result = arr_index_of_first_max(arr1);
+	cout << "\n\nThe index of the first max value in the array \"arr1\" is: " << result;
+
+	// Test arr_index_of_last_max() function
+    result = arr_index_of_last_max(arr1);
+	cout << "\n\nThe index of the last max value in the array \"arr1\" is: " << result;
+
+	// Test arr_index_of_value_first() function
+    result2 = arr_index_of_value_first(arr1, 15);
+	cout << "\n\nThe index of the first occurrence of 15 in the array \"arr1\" is: " << result2;
+	result2 = arr_index_of_value_first(arr1, 100);
+	cout << "\nThe index of the first occurrence of 100 in the array \"arr1\" is: " << result2;
+
+	// Test arr_index_of_value_last() function
+    // change result2 to size_t could ease implicit conversion warning, but it won't return value of -1 since it returns unsigned int;
+    result2 = arr_index_of_value_last(arr1, 15);
+	cout << "\n\nThe index of the last occurrence of 15 in the array \"arr1\" is: " << result2;
+    result2 = arr_index_of_value_last(arr1, 100);
+	cout << "\nThe index of the last occurrence of 100 in the array \"arr1\" is: " << result2;
+
+	// Test arr_compare() function
+    result3 = arr_compare(arr1, arr3);
+	if (result3)
+	{
+		cout << "\n\nThe arrays \"arr1\" and \"arr2\" are equal.";
+	}
+	else
+	{
+		cout << "\n\nThe arrays \"arr1\" and \"arr2\" are NOT equal.";
+	}
+
+	
+	if (result3)
+	{
+		cout << "\n\nThe arrays \"arr1\" and \"arr3\" are equal.\n\n";
+	}
+	else
+	{
+		cout << "\n\nThe arrays \"arr1\" and \"arr3\" are NOT equal.\n\n";
+	}
+
+	system("pause");
+}
+
+// Search array and return the max value in the array
+template <size_t N>
+int arr_max(const array<int, N> &a)
+{
+    int maxValue = a[0];
+    for(size_t i = 1; i < a.size(); i++){
+        if(a[i] > maxValue){
+            maxValue = a[i];
+        }
+    }
+    return maxValue;
+}
+
+// Search array and return the index of the max value in the array
+template <size_t N>
+size_t arr_index_of_first_max(const array<int, N> &a)
+{
+    
+    int maxValue = a[0];
+    size_t index =0;
+    for(size_t i = 1; i < a.size(); i++){
+        if(a[i] > maxValue){
+            maxValue = a[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+// Search array and return the index of the last max value in the array
+template <size_t N>
+size_t arr_index_of_last_max(const array<int, N> &a)
+{
+    int maxValue = a[0];
+    size_t index = 0;
+    for(size_t i = 1; i < a.size(); i++){
+        if(a[i] >= maxValue && i > index){
+            maxValue = a[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+// Search array and return index of first occurence of value in the array
+// Return -1 if value is not in the array
+template <size_t N>
+size_t arr_index_of_value_first(const array<int, N> &a, int value)
+{
+
+    for(size_t i = 0; i < a.size(); i++){
+        if(a[i] == value){
+            return i;
+        }
+    }
+    
+    return -1;
+}
+
+// Search array and return index of last occurence of value in the array
+// Return -1 if value is not in the array
+template <size_t N>
+
+//changed return type from "int" to "size_t"
+//cstyle file int type go through, stl file int type build failed... <--------Question
+
+size_t arr_index_of_value_last(const array<int, N> & a, int value)
+{
+    //function has to return size_t, index use type int, int -> size_t (unsigned int) avoid program fail;
+    int index = -1;
+    for (unsigned int i = 0; i < a.size(); ++i)
+    {
+        if (a[i] == value)
+        {
+            //compiler won't conpile unsigned int i convert to int, implicit conversion unsigned int to int (large to small);
+            index = i;
+        }
+    }
+
+    return index;
+}
+
+// Compare 2 arrays and return true if they are equal (true) and false (false) otherwise
+// You can assume that the 2 arrays are the same size
+template <size_t N>
+bool arr_compare(const array<int, N> &a, const array<int, N> &b)
+{
+//    SIZE1 used for arr1 & arr3
+//    if(a.size() != b.size()){
+//        return false;
+//    }
+    
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+
